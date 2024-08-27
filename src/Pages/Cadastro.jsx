19555d4aba76946274f4563a006de1a490d1251a
Fabricio Belam <36649120+fbelam@users.noreply.github.com>
2024-08-26 21:24:27 -0300

    Update Cadastro.jsx

--- src/Pages/Cadastro.jsx
+++ src/Pages/Cadastro.jsx
@@ -1,26 +1,41 @@
-import React from 'react';
-import styled from "styled-components";
+import styled from 'styled-components'
+
+const Container = styled.div`
+  width: 100%;
+  max-width: 100%;
+  margin-top: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items:center;
+  gap: 10px;
+`;
 
 const FormsContainer = styled.form`
+    margin-left: 10%;
     display: flex;
-    align-items: flex-end;
     background-color: black;
     gap: 10px;
     flex-wrap:  wrap;
     background-color: #fff;
-    padding: 20px;
+    padding: 200px;
     box-shadow: 0px 0px 5px #ccc;
     border-radius: 5px;
+    flex-direction: column;
+    width: 100%;
+    background-color: rgba(255, 255, 255, 0.2); /* Cor branca com 20% de opacidade */
+    backdrop-filter: blur(10px); /* Adiciona um efeito de desfoque de fundo */
+    color: #0a0909; /* Texto branco */
 `;
 
 const InputArea = styled.div`
     display: flex;
     flex-direction: column;
+    background-color: #ddddff;
     `;
 
-const input = styled.input`
-    width: 120px;
-    padding: 0 10px;
+const Input = styled.input`
+    width: 80%;
+    padding: 0 10%;
     border: 1px solid #bbb;
     border-radius: 5px;
     height: 40px;
@@ -28,19 +43,70 @@ const input = styled.input`
 
 const Label = styled.label``;
 
-const formulario = () => {
+function Form() {
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log('You clicked submit.');
+  }
 
-    return (
+  return (
+    <form onSubmit={handleSubmit}>
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
 
-        <FormsContainer>
-            <h2>Cadastro</h2>
+function App(){
+  return (
+    <>
+      <h2>Cadastro Projetista</h2>
+      <Container>
+      <FormsContainer>
+      <InputArea>
+        <Label>Nome</Label>
+        <Input name="nome" />
+      </InputArea>
+      <InputArea>
+        <Label>CPF</Label>
+        <Input name="cpf"/>
+      </InputArea>
+      <InputArea>
+        <Label>E-mail</Label>
+        <Input name="email" type='email'/>
+      </InputArea>
+      <InputArea>
+        <Label>Telefone</Label>
+        <Input name="telefone" />
+      </InputArea>
+      <InputArea>
+        <Label>CEP</Label>
+        <Input name="cep" />
+      </InputArea>
+      <InputArea>
+        <Label>Endereço</Label>
+        <Input name="logradouro" />
+      </InputArea>
+      <InputArea>
+        <Label>Numero</Label>
+        <Input name="numero"/>
+      </InputArea>
+      <InputArea>
+        <Label>Cidade</Label>
+        <Input name="cidade"/>
+      </InputArea>
+      <InputArea>
+        <Label>UF</Label>
+        <Input name="uf"/>
+      </InputArea>
+      <InputArea>
+        <Label>Complemento</Label>
+        <Input name="complemento"/>
+      </InputArea>
+      <Form/>
+      </FormsContainer>    
+      </Container>
+    </>
+  )
+}
 
-            <InputArea>
-            <Label>Nome</Label>
-            <input name="nome" />
-            </InputArea>
-        </FormsContainer>
-    );
-};
-
-export default formulario;
+export default App
